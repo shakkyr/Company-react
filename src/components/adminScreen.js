70b3602cecc92c7b7816c52af9c2d9cbf18aa206
Shadi Rada <shakkyr@gmail.com>
2021-10-31 09:04:16 +0200

    company

--- src/components/adminScreen.js
+++ src/components/adminScreen.js
@@ -1,7 +1,57 @@
 import React from "react";
 
-const Shadi = () => {
-  return <div>shadi rada</div>;
+export const Shadi = () => {
+  return (
+    <div className="div-home">
+      <section id="about-info" className="bg-light py-3">
+        <div className="container">
+          <div className="info-left">
+            <h1 className="l-heading">
+              <span className="text-primary">Im </span> Shadi Rada{" "}
+            </h1>
+            <p>
+              established this mini company to rise up the world hiegher forher.
+            </p>
+            <p>
+              Join me to build the future
+            </p>
+          </div>
+          <div className="info-right">
+            <img
+              src='./'
+              alt="hotel"
+            />
+          </div>
+        </div>
+      </section>
+      <div className="clr"></div>
+      <section id="testimonials" className="py-3">
+        <div className="container">
+          <h2 className="l-heading">What Our costumers Say</h2>
+          <div className="testimonial bg-primary">
+          
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
+              eligendi quibusdam, dolorum maxime cum numquam quisquam, deleniti
+              eum incidunt, velit non consectetur. Facere, ipsa maxime, ullam id
+              amet odio laboriosam sit iusto tempore fugit exercitationem, a
+              dolore quo maiores nisi!
+            </p>
+          </div>
+          <div className="testimonial bg-primary">
+           
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
+              eligendi quibusdam, dolorum maxime cum numquam quisquam, deleniti
+              eum incidunt, velit non consectetur. Facere, ipsa maxime, ullam id
+              amet odio laboriosam sit iusto tempore fugit exercitationem, a
+              dolore quo maiores nisi!
+            </p>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Shadi;
